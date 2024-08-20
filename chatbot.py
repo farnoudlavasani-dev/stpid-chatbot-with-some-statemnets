13b9827b3b4de5ba6de315e543ce40da99d984d8
farnoudlavasani-dev <lavasanifarnoud@gmail.com>
2024-08-20 17:47:24 +0330

    simpe chatbot commit

---
+++ chatbot.py
@@ -0,0 +1,22 @@
+
+def reply(message):
+    if "hello" in message or "hi" in message or "hey" in message:
+        reply = "Hi there!"
+    elif "who" in message and "you" in message:
+        reply = "I am a chatbot with if-else statements, please be kind to me."
+    elif "joke" in message:
+        reply = "There are 10 types of people in the world: those who understand binary and those who don't."
+    else:
+        reply = "I'm still learning; one day I can answer this message."
+    return reply
+
+message = input("Human: " ).lower()
+while "exit" not in message:
+    if "bye" in message:
+        print("Have a good day!")
+        break
+    else:
+        print("Robot: " + reply(message))
+        message = input("Human: ")
+        print("To exit the program, just say 'exit'.")
+    
